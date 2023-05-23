8e4e9a275b6f991b073e6256590c529736d0956e
joolen_h-onozuka <h-onozuka@joolen.com>
2023-05-23 15:07:32 +0900

    店舗一覧、ブランドストーリ

---
+++ assets/customize.js
@@ -0,0 +1,20 @@
+/* ブランドストーリ */
+function fadeAnime(){
+    $('.fadeInTrigger').each(function(){ 
+		var elemPos = $(this).offset().top-50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeIn');
+		}else{
+		$(this).removeClass('fadeIn');
+		}
+		});
+}
+
+$(window).scroll(function (){
+    fadeAnime();
+});
+$(window).on('load', function(){
+    fadeAnime();
+});
